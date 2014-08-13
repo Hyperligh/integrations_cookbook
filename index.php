0d072032c363e31cb5cd5e45962f480bd3e4dbba
Hyperligh <hyperligh19@hotmail.com>
2014-08-13 14:58:21 -0700

    Opened with sublime for CLI

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
-<html landg="en-US">
+<html lang="en-US">
 <head>
 	<meta name="author" content="Josh Chambers">
 	<meta name="viewport" content="width=device-width">
 	<meta charset="utf-8">
-	<title>Integrations Cookbook</title>
+	<meta name="description" content="Did you want to know everything about all the intragrations that G5 deals with? Yes, of course.">
+	<meta name="keywords" content="Integrations Cookbook, Integrations, Integrations Workbook, G5, G5 Search Marketing, DXM">
+	<title>Integrations CookBook</title>
+	<link rel="stylesheet" href="style/integrations_cookbook.css">
 </head>
 <body>
 <div id="container">
 <h1>Integrations CookBook</h1>
-<p>All you need to know about Integrations</p>
+<h3>Everything you need to know about integrations</h3>
+
 </div>
 </body>
 </html>
