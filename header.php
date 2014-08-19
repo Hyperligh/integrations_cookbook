5ee34d3ecd65a0c4d4b4e8198e8ce957290d1604
Hyperligh <hyperligh19@hotmail.com>
2014-08-18 20:35:06 -0700

    Pieceing the constructor together

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <header>
 	<nav>
 		<ul>
-			<li><a href="#">SiteLink</a></li>
-			<li><a href="#">Yardi RentCafe</a></li>
-			<li><a href="#">Yardi Voyager</a></li>
-			<li><a href="#">Yardi Senior Living</a></li>
-			<li><a href="#">CenterShift</a></li>
+			<li><a href="index.php?id=1">SiteLink</a></li>
+			<li><a href="index.php?id=2">Yardi RentCafe</a></li>
+			<li><a href="index.php?id=3">Yardi Voyager</a></li>
+			<li><a href="index.php?id=4">Yardi Senior Living</a></li>
+			<li><a href="index.php?id=5">CenterShift</a></li>
 		</ul>
 	</nav>
 </header>
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-require_once'head.php';
-require_once'header.php';
-require_once'home.html';
+if(!empty($_GET['id'])){
+	if($_GET['id'] == 1){
+		require_once'head.php';
 
-require_once'footer.php';
+
+
+		require_once'footer.php';
+	}//if id = 1 SiteLink
+	else {
+		require_once'head.php';
+		require_once'header.php';
+		require_once'home.html';
+
+		require_once'footer.php';
+	}
+}//if there is an id
 ?>
