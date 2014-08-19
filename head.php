ce54ef528b99eaea7d5c2f0a6914da5f95ba209d
Hyperligh <hyperligh19@hotmail.com>
2014-08-18 20:04:56 -0700

    Added more construct to the view

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta name="author" content="Josh Chambers">
+	<meta name="viewport" content="width=device-width">
+	<meta charset="utf-8">
+	<meta name="description" content="Did you want to know everything about all the intragrations that G5 deals with? Yes, of course.">
+	<meta name="keywords" content="Integrations Cookbook, Integrations, Integrations Workbook, G5, G5 Search Marketing, DXM">
+	<title>Integrations CookBook</title>
+	<link rel="stylesheet" href="style/cookbook_style.css">
+</head>
+<body>
+<div id="container">
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
+require_once'head.php';
 require_once'header.php';
-
-
+require_once'home.html';
 
 require_once'footer.php';
 ?>
