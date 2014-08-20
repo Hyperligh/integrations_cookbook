51d8deb5729fd2c6927d2c27fd9d21c2d7aa2da0
Hyperligh <hyperligh19@hotmail.com>
2014-08-19 21:44:02 -0700

    Working MVC framework

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
-if(!empty($_GET['id'])){
+//if session == client 
+if(!isset($_GET['id'])){
+		require_once'head.php';
+		require_once'header.php';
+		require_once'home.html';
+		require_once'footer.php';
+
+	}else{
 	if($_GET['id'] == 1){
 		require_once'head.php';
-
+		require_once'header.php';
+		echo"<h1>Inside SITELINK</h1>";
+		
 
 
 		require_once'footer.php';
 	}//if id = 1 SiteLink
-	else {
+	elseif ($_GET['id'] == 2) {
 		require_once'head.php';
 		require_once'header.php';
-		require_once'home.html';
+		echo"<h1>Inside RentCafe</h1>";
 
 		require_once'footer.php';
+
 	}
+	
 }//if there is an id
+//else if session == g5 
 ?>
